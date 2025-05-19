b68c93188a2605cb4491bb4f8a44f054d8454fc0
Pragalbhsh <pragalbhsharma11@gmail.com>
2025-05-19 19:36:16 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const request = require("request")
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
+
+const input = process.argv[2]
+if(!input){
+    console.log("please give an address")
+}
+else
+{
+
+geocode( input ,(error,{latitude, longitude ,location}) =>{
+    if(error){
+        return console.log(error)
+    }
+  
+    forecast(latitude,longitude,(error,data)=>{
+        if (error){
+            return console.log(error)
+        }
+   
+    console.log(location)
+    console.log(data)
+   
+    
+        
+      })
+})
+}
+
+
---
+++ utils/forecast.js
@@ -0,0 +1,22 @@
+const request  = require("request")
+
+const forecast = (lat,lon,callback)=>{
+    const url = "http://api.weatherapi.com/v1/current.json?key=cf2a5ebd11c54eb894d184924241906&query="+lat+","+lon
+
+request({url ,json:true},(error,{body})=>{
+    if(error){
+        callback("unable to connect to the network" , undefined)
+    }
+    else if(body.current === undefined){
+        callback("wrong inputs",undefined)
+    }
+    else{
+        callback(undefined,body.current.condition.text + " . it is currently " + body.current.temp_c + " but feels like " + body.current.feelslike_c) 
+    }
+}
+
+
+)
+}
+
+module.exports = forecast
